d8ae17ee512a2ceb35b72d095a87b38c48eca220
Евгения Ярош <102187246+Yarosh17@users.noreply.github.com>
2022-03-24 16:40:52 +0000

    Введення масиву

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ int main()
 { double a[9]; int i, k=0;
 cout<<"Введіть 9 дійсних чисел:\n";
 for(int i=0;i<9;i++)
-{//Ведення масиву
+{cin >> a[i];
  //Перевірка
   k++;}
 cout << "k = " << k << endl;
